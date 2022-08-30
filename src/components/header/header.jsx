4d1afed37587f4afa379cca5131127e5cd690db6
Jovliyev Inomjon <91968320+Inomjon7777@users.noreply.github.com>
2022-08-30 21:45:29 +0500

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+import "./header.scss";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="logo">
+          <h1>
+            CodingBat <span>code practice</span>
+          </h1>
+          <p>
+            Welcome to Codingbat. See <a href="##">help</a> for the latest.
+          </p>
+        </div>
+        <div className="validation">
+          <ul>
+            {["about", "help", "code help+videos", "done", "prefs"].map(
+              (item) => (
+                <li>{item}</li>
+              )
+            )}
+          </ul>
+          <form>
+            <label htmlFor="email">
+              id/email
+              <input type="email" />
+            </label>
+            <label htmlFor="password">
+              password
+              <input type="password" />
+            </label>
+            <button>log in</button>
+            <ul>
+              {["forgot password", "create account"].map((item) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+          </form>
+        </div>
+      </header>
+    );
+  }
+}
