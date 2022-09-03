caadf9b6c8e6403e25a0b4cc668817dfb600f0f8
Jovliyev Inomjon <91968320+Inomjon7777@users.noreply.github.com>
2022-09-03 21:39:33 +0500

    weather-app add

---
+++ script.js
@@ -0,0 +1,137 @@
+//Hamma elementlar DOM dan js faylga chaqirib olindi
+const app = document.querySelector('.weather-app');
+const temp = document.querySelector('.temp');
+const dateOutput = document.querySelector('.date');
+const timeOutput = document.querySelector('.time');
+const conditionOutput = document.querySelector('.condition');
+const nameOutput = document.querySelector('.name');
+const icon = document.querySelector('.icon');
+const cloudOutput = document.querySelector('.cloud');
+const humidityOutput = document.querySelector('.humidity');
+const windOutput = document.querySelector('.wind');
+const form = document.querySelector('#locationInput');
+const search = document.querySelector('.search');
+const btn = document.querySelector('.submit');
+const cities = document.querySelectorAll('.city');
+
+//Sahifa yuklanganda default shahar chiqishi
+let cityInput = "London";
+
+//Paneldagi har bir shaharga click hodisasini qo'shish
+cities.forEach((city) => {
+    // standart shahardan bosilgan shaharga o'zgartirish
+    city.addEventListener('click', (e) => {
+        cityInput = e.target.innerHTML;
+        fetchWeatherData();
+        app.style.opacity = "0";
+    })
+});
+
+//Formga submit eventini qo'shish
+form.addEventListener('submit', (e) => {
+    if (search.value.length == 0) {
+        alert('Please type in a city name');
+    } else {
+        cityInput = search.value;
+        fetchWeatherData();
+        search.value = "";
+        app.style.opacity = "0";
+    }
+    e.preventDefault();
+});
+
+function dayOfTheWeek(day, month, year) {
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return weekday[new Data(`${day}/${month}/${year}`).getDay()];
+}
+
+function fetchWeatherData() {
+    //**************************************************** */
+    fetch(`http//api.weatherapi.com/
+    v1/current.json?key==${cityInput}`)
+        //**************************************************** */
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            timeOutput.innerHTML = data.current.temp_c + "&#176;";
+            conditionOutput.innerHTML = data.current.condition.text;
+
+            const date = data.location.localtime;
+            const y = parseInt(date.substr(0, 4));
+            const m = parseInt(date.substr(5, 2));
+            const time = date.substr(11);
+        })
+
+    dateOutput.innerHTML = `${dayOfTheWeek(d,m,y)} ${d},${m},${y}`;
+    timeOutput.innerHTML = time;
+
+    nameOutput.innerHTML = data.location.name;
+
+    const iconId = data.current.condition.icon.substr(
+        "//cdn.weatherapi.com/weather/64x64/".length);
+
+    icon.src = "./icons/" + iconId;
+
+    cloudOutput.innerHTML = data.current.cloud + "%";
+    humidityOutput.innerHTML = data.current.humidity + "%";
+    windOutput.innerHTML = data.current.wind_kph + "km/h";
+
+    let timeOfDay = "day";
+
+    const code = data.current.condition.code;
+
+    if (!data.current.is_day) {
+        timeOfDay = "night";
+    }
+
+    if (code == 1000) {
+        app.style.backgroundImage = `url(./images/${timeOfDay}/clear.jpg)`
+        btn.style.background = "#e5ba92";
+        if (timeOfDay = "night") {
+            btn.style.background = "#181e27";
+
+        }
+    } else if (
+        code == 1003 ||
+        code == 1006 ||
+        code == 1009 ||
+        code == 1030 ||
+        code == 1069 ||
+        code == 1087 ||
+        code == 1135 ||
+        code == 1273 ||
+        code == 1276 ||
+        code == 1279 ||
+        code == 1282
+    ) {
+        app.style.backgroundImage = `url(./images/${timeOfDay}/cloudy.jpg)`;
+        btn.style.background = "#fa6d1b";
+        if (timeOfDay == "night") {
+            btn.style.background = "#181e27";
+        } else if (
+            code == 1003 ||
+            code == 1006 ||
+            code == 1009 ||
+            code == 1030 ||
+            code == 1069 ||
+            code == 1087 ||
+            code == 1135 ||
+            code == 1273 ||
+            code == 1276 ||
+            code == 1279 ||
+            code == 1282
+        ) {
+            app.style.background = `url(./images/${timeOfDay}/rainy.jpg)`;
+            btn.style.background = "#647d75";
+            if (timeOfDay == "night") {
+                btn.style.background = "#181e27";
+            }
+
+        }
+    }
+    app.style.opacity = "1";
+
+    fetchWeatherData();
+    app.style.opacity = "1";
+
+};
