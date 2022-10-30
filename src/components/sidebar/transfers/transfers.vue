af56df67fa83e16ac1707e6356f685f8fd810f8a
Jovliyev Inomjon <91968320+Inomjon7777@users.noreply.github.com>
2022-10-30 18:13:56 +0500

    i finish transfers box

--- src/components/sidebar/transfers/transfers.vue
+++ src/components/sidebar/transfers/transfers.vue
@@ -10,69 +10,75 @@
           Kartadan kartaga o’tkazish
         </p>
         <p class="transfer-from-card-to-card__description">Xizmat narxi 1%</p>
-        <div class="transfer-box">
-          <!-- transfer__buttons box -->
-          <div class="transfer__buttons-box">
-            <button>
-              <img :src="iconTransferMoney" alt="icon_transfer_money" />
-              Pul o‘tkazish
-            </button>
-            <button>
-              <img :src="iconAskingForMoney" alt="iconAskingForMoney" /> Pul
-              so‘rash
-            </button>
-            <button>
-              <img :src="iconTransferToMyCard" alt="iconTransferToMyCard" />
-              Mening kartamga o‘tkazish
-            </button>
-          </div>
-          <!-- ecipients card or phone number box -->
-          <div class="recipients-card-or-phone-number-box">
-            <p class="recipients-card-or-phone-number-box__title">
-              Qabul qiluvchi kartaning yoki telefon raqami
-            </p>
-            <div class="recipients-card-or-phone-number-box__input-box">
-              <img
-                class="iconRecipientsCard"
-                :src="iconRecipientsCard"
-                alt="icon_recipients_card"
-              />
-              <input placeholder="0000 0000 0000 0000" />
-              <img
-                class="iconUserSquare"
-                :src="iconUserSquare"
-                alt="icon_user_square"
-              />
+        <div class="transfer-box-parent">
+          <div class="transfer-box">
+            <!-- transfer__buttons box -->
+            <div class="transfer__buttons-box">
+              <button>
+                <img :src="iconTransferMoney" alt="icon_transfer_money" />
+                Pul o‘tkazish
+              </button>
+              <button>
+                <img :src="iconAskingForMoney" alt="iconAskingForMoney" /> Pul
+                so‘rash
+              </button>
+              <button>
+                <img :src="iconTransferToMyCard" alt="iconTransferToMyCard" />
+                Mening kartamga o‘tkazish
+              </button>
             </div>
-          </div>
-          <!-- transfer amount box -->
-          <div class="transfer-amount-box">
-            <p class="transfer-amount-box__title">O’tkazma summasi</p>
-            <div class="transfer-amount-box__input-box">
-              <input type="text" placeholder="Summani kiriting" />
-              <p>so'm</p>
-            </div>
-          </div>
-          <!-- plastic card box -->
-          <div class="plastic-card-box">
-            <p class="plastic-card-box__title">O’tkazma summasi</p>
-            <div class="plastic-card-box__card-box">
-              <div class="icon-uzcard-box">
-                <img class="iconUzcard" :src="iconUzcard" alt="icon_two_card" />
+            <!-- ecipients card or phone number box -->
+            <div class="recipients-card-or-phone-number-box">
+              <p class="recipients-card-or-phone-number-box__title">
+                Qabul qiluvchi kartaning yoki telefon raqami
+              </p>
+              <div class="recipients-card-or-phone-number-box__input-box">
+                <img
+                  class="iconRecipientsCard"
+                  :src="iconRecipientsCard"
+                  alt="icon_recipients_card"
+                />
+                <input placeholder="0000 0000 0000 0000" />
+                <img
+                  class="iconUserSquare"
+                  :src="iconUserSquare"
+                  alt="icon_user_square"
+                />
               </div>
-              <div class="plastic-card-box__card-box__word-box">
-                <p>8600 14** **** 1816</p>
-                <p>2 457 844.00 UZS</p>
-              </div>
-              <img
-                class="icon_vertical_strelka"
-                :src="iconVerticalStrelka"
-                alt="icon_vertical_strelka"
-              />
             </div>
+            <!-- transfer amount box -->
+            <div class="transfer-amount-box">
+              <p class="transfer-amount-box__title">O’tkazma summasi</p>
+              <div class="transfer-amount-box__input-box">
+                <input type="text" placeholder="Summani kiriting" />
+                <p>so'm</p>
+              </div>
+            </div>
+            <!-- plastic card box -->
+            <div class="plastic-card-box">
+              <p class="plastic-card-box__title">O’tkazma summasi</p>
+              <div class="plastic-card-box__card-box">
+                <div class="icon-uzcard-box">
+                  <img
+                    class="iconUzcard"
+                    :src="iconUzcard"
+                    alt="icon_two_card"
+                  />
+                </div>
+                <div class="plastic-card-box__card-box__word-box">
+                  <p>8600 14** **** 1816</p>
+                  <p>2 457 844.00 UZS</p>
+                </div>
+                <img
+                  class="icon_vertical_strelka"
+                  :src="iconVerticalStrelka"
+                  alt="icon_vertical_strelka"
+                />
+              </div>
+            </div>
+            <!-- button the next step -->
+            <div class="the-next-step-box"><button>Keyingi qadam</button></div>
           </div>
-          <!-- button the next step -->
-          <div class="the-next-step-box"><button>Keyingi qadam</button></div>
         </div>
       </div>
       <!---HISTORY OF TRANSFERS--->
@@ -91,10 +97,12 @@
           </div>
           <!-- 1 history-of-transfers-box__card-box -->
           <div class="history-of-transfers-box__card-box">
-            <img :src="iconTwoCard" alt="icon_two_card" />
-            <div class="card-name-vs-date">
-              <p>Kamolon</p>
-              <p>12.06.2022 14:06</p>
+            <div class="history-of-transfers-box__card-box__img-vs-date-box">
+              <img :src="iconTwoCard" alt="icon_two_card" />
+              <div class="card-name-vs-date">
+                <p>Kamolon</p>
+                <p>12.06.2022 14:06</p>
+              </div>
             </div>
             <div class="cut-off-amount-vs-card-number">
               <p>- 51 245</p>
@@ -103,22 +111,26 @@
           </div>
           <!-- 2 history-of-transfers-box__card-box -->
           <div class="history-of-transfers-box__card-box">
-            <img :src="iconTwoCard" alt="icon_two_card" />
-            <div class="card-name-vs-date">
-              <p>Kamolon</p>
-              <p>12.06.2022 14:06</p>
+            <div class="history-of-transfers-box__card-box__img-vs-date-box">
+              <img :src="iconTwoCard" alt="icon_two_card" />
+              <div class="card-name-vs-date">
+                <p>Kartadan kartaga o’tk...</p>
+                <p>12.06.2022</p>
+              </div>
             </div>
             <div class="cut-off-amount-vs-card-number">
-              <p>- 51 245</p>
+              <p class="green-summa">+ 4 144 254.00 UZS</p>
               <p>8600 01** **** 1346</p>
             </div>
           </div>
           <!-- 3 history-of-transfers-box__card-box -->
           <div class="history-of-transfers-box__card-box">
-            <img :src="iconTwoCard" alt="icon_two_card" />
-            <div class="card-name-vs-date">
-              <p>Kamolon</p>
-              <p>12.06.2022 14:06</p>
+            <div class="history-of-transfers-box__card-box__img-vs-date-box">
+              <img :src="iconTwoCard" alt="icon_two_card" />
+              <div class="card-name-vs-date">
+                <p>Kamolon</p>
+                <p>12.06.2022 14:06</p>
+              </div>
             </div>
             <div class="cut-off-amount-vs-card-number">
               <p>- 51 245</p>
@@ -131,10 +143,12 @@
           </div>
           <!-- 4 history-of-transfers-box__card-box -->
           <div class="history-of-transfers-box__card-box">
-            <img :src="iconTwoCard" alt="icon_two_card" />
-            <div class="card-name-vs-date">
-              <p>Kamolon</p>
-              <p>12.06.2022 14:06</p>
+            <div class="history-of-transfers-box__card-box__img-vs-date-box">
+              <img :src="iconTwoCard" alt="icon_two_card" />
+              <div class="card-name-vs-date">
+                <p>Kamolon</p>
+                <p>12.06.2022 14:06</p>
+              </div>
             </div>
             <div class="cut-off-amount-vs-card-number">
               <p>- 51 245</p>
@@ -143,13 +157,15 @@
           </div>
           <!-- 5 history-of-transfers-box__card-box -->
           <div class="history-of-transfers-box__card-box">
-            <img :src="iconTwoCard" alt="icon_two_card" />
-            <div class="card-name-vs-date">
-              <p>Kamolon</p>
-              <p>12.06.2022 14:06</p>
+            <div class="history-of-transfers-box__card-box__img-vs-date-box">
+              <img :src="iconTwoCard" alt="icon_two_card" />
+              <div class="card-name-vs-date">
+                <p>Kartadan kartaga o’tk...</p>
+                <p>12.06.2022</p>
+              </div>
             </div>
             <div class="cut-off-amount-vs-card-number">
-              <p>- 51 245</p>
+              <p class="green-summa">+ 4 144 254.00 UZS</p>
               <p>8600 01** **** 1346</p>
             </div>
           </div>
@@ -188,13 +204,25 @@ export default {
 
 <style scoped>
 .transfers {
-  background-color: #fafafa;
+  background-color: #f0f0f0;
+  height: 655px;
 }
 .transfers-boxes {
   display: flex;
+  height: 585px;
+  justify-content: space-between;
+  align-items: center;
 }
 .transfer-from-card-to-card {
   margin: 24px;
+  height: 585px;
+  flex-grow: 7;
+}
+.history-of-transfers {
+  flex-grow: 3;
+  height: 545px;
+  margin-right: 24px;
+  width: 28%;
 }
 .transfer-from-card-to-card,
 .history-of-transfers {
@@ -217,12 +245,18 @@ export default {
   font-size: 14px;
   color: #919191;
 }
+.transfer-box-parent {
+  display: grid;
+  place-items: center;
+}
 .transfer-box {
   background: #ffffff;
   border: 1px solid #e9e9e9;
   border-radius: 10px;
-  padding: 25px 33px;
-  margin: 50px;
+  padding: 15px;
+  height: 430px;
+  width: max-content;
+  margin-top: 20px;
 }
 .transfer-from-card-to-card__title {
   font-family: "Gilroy";
@@ -254,34 +288,15 @@ export default {
   justify-content: center;
   align-items: center;
   transition: all 0.5s ease;
-}
-
-.transfer__buttons-box button:first-child {
-  font-family: "SF Pro Display";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  color: #929292;
-  padding: 14px 32px;
-  background: #f0f0f0;
-  border-radius: 10px;
-  border: 1px solid #f0f0f0;
-  cursor: pointer;
-  display: flex;
-  gap: 10px;
-}
-
-.transfer__buttons-box button:nth-child(2),
-.transfer__buttons-box button:last-child {
   cursor: pointer;
   display: flex;
   gap: 10px;
   font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 500;
-  font-size: 16px;
+  font-size: 12px;
   color: #929292;
-  padding: 14px 32px;
+  padding: 6px 16px;
   background: #f0f0f0;
   border-radius: 10px;
   border: 1px solid #f0f0f0;
@@ -296,18 +311,19 @@ export default {
   font-family: "Gilroy";
   font-style: normal;
   font-weight: 600;
-  font-size: 16px;
+  font-size: 12px;
   color: #021723;
-  margin-top: 33px;
+  margin-top: 20px;
 }
 .recipients-card-or-phone-number-box__input-box {
   border: 1px solid #d5e3ed;
   border-radius: 10px;
   display: flex;
   align-items: center;
+  width: 500px;
 }
 .iconUserSquare {
-  padding: 14px;
+  padding: 7px;
   background: #fbab07;
   border-radius: 0px 10px 10px 0px;
 }
@@ -317,7 +333,7 @@ export default {
 }
 .recipients-card-or-phone-number-box__input-box {
   margin-top: 8px;
-  margin-bottom: 26px;
+  margin-bottom: 20px;
 }
 .recipients-card-or-phone-number-box__input-box input {
   border: none;
@@ -325,7 +341,7 @@ export default {
   font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 400;
-  font-size: 16px;
+  font-size: 12px;
   color: #9597a8;
   outline: none;
 }
@@ -333,14 +349,17 @@ export default {
   font-family: "Gilroy";
   font-style: normal;
   font-weight: 600;
-  font-size: 16px;
+  font-size: 12px;
   color: #021723;
 }
 .transfer-amount-box__input-box {
   display: flex;
   border: 1px solid #d5e3ed;
   border-radius: 10px;
-  padding: 16.5px 16.5px 16.5px 17.5px;
+  padding: 7px;
+  width: 485px;
+  margin-top: 8px;
+  margin-bottom: 20px;
 }
 .transfer-amount-box__input-box input {
   width: 100%;
@@ -350,14 +369,15 @@ export default {
   font-family: "Gilroy";
   font-style: normal;
   font-weight: 600;
-  font-size: 16px;
+  font-size: 12px;
   color: #021723;
 }
 .plastic-card-box__card-box {
   display: flex;
-  padding: 19px 17px;
+  padding: 7px;
   border: 1px solid #d5e3ed;
   border-radius: 10px;
+  width: 485px;
 }
 .iconUzcard {
   width: 33px;
@@ -373,8 +393,9 @@ export default {
   font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 500;
-  font-size: 16px;
+  font-size: 13px;
   color: #000000;
+  margin-top: 7px;
 }
 .plastic-card-box__card-box__word-box {
   margin-left: 16px;
@@ -383,24 +404,24 @@ export default {
   font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 700;
-  font-size: 24px;
+  font-size: 20px;
   color: #000000;
 }
 .icon_vertical_strelka {
-  margin-left: 350px;
+  margin-left: 210px;
 }
 .the-next-step-box {
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 33px;
+  margin-top: 15px;
 }
 .the-next-step-box button {
-  padding: 16.5px 35.5px;
+  padding: 10px 20px;
   font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 500;
-  font-size: 16px;
+  font-size: 12px;
   color: #ffffff;
   background: #fbab07;
   border-radius: 10px;
@@ -414,6 +435,10 @@ export default {
 }
 
 /*HISTORY OF TRANSFERS*/
+.transfers__title {
+  padding: 10px 0px 14px 0px;
+  margin-left: 24px;
+}
 .history-of-transfers {
   padding: 20px 22px;
 }
@@ -426,14 +451,17 @@ export default {
 }
 .history-of-transfers-box__date-box {
   display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .history-of-transfers-box__date-box p:first-child {
   font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 700;
-  font-size: 12px;
+  font-size: 16px;
   letter-spacing: -0.02em;
   color: #000000;
+  margin-top: 16px;
 }
 .green-summa,
 .red-summa,
@@ -441,7 +469,7 @@ export default {
   font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 500;
-  font-size: 10px;
+  font-size: 16px;
   letter-spacing: -0.02em;
 }
 .green-summa {
@@ -459,12 +487,28 @@ export default {
 }
 .history-of-transfers-box__card-box {
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 16px;
+  padding: 6px 7px;
+  background: #ffffff;
+  box-shadow: 0px 0px 20px rgba(0, 4, 106, 0.03);
+  border-radius: 5px;
+}
+.history-of-transfers-box__card-box img {
+  width: 24px;
+  height: 24px;
+}
+.card-name-vs-date {
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
 }
 .card-name-vs-date p:first-child {
   font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 700;
-  font-size: 12px;
+  font-size: 15px;
   letter-spacing: -0.02em;
   color: #022964;
 }
@@ -476,11 +520,17 @@ export default {
   letter-spacing: -0.02em;
   color: #6a7494;
 }
+.cut-off-amount-vs-card-number {
+  display: flex;
+  gap: 6px;
+  flex-direction: column;
+  align-items: flex-end;
+}
 .cut-off-amount-vs-card-number p:last-child {
   font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 500;
-  font-size: 10px;
+  font-size: 12px;
   letter-spacing: -0.02em;
   color: #6a7494;
 }
@@ -488,8 +538,23 @@ export default {
   font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 700;
-  font-size: 12px;
+  font-size: 15px;
   letter-spacing: -0.02em;
   color: #dc3027;
+}
+
+.history-of-transfers-box__card-box__img-vs-date-box {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+.history-of-transfers-box__card-box__img-vs-date-box img {
+  padding: 12px;
+  background: #ffffff;
+  box-shadow: 0px 0px 20px rgba(0, 4, 106, 0.05);
+  border-radius: 5px;
+}
+.green-summa {
+  color: #00b488 !important;
 }
 </style>
